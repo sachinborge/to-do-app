4054c8ca6b9ae124c88440fb14728c00797e5499
sachin <borgesachin365@gmail.com>
2021-09-01 16:46:34 +0530

    my first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+<div style={{height:"100vh",width:"100%",display:"flex",justifyContent:"center",alignItems:"center"}}>
+<h1 style={{color:"red",fontSize:"65px"}}>Hello World</h1>
+</div>
+  );
+}
+
+export default App;
